52e93fe1b6a7c4363fdf84309639914eaf1b9ba5
manognav <manognav@manognas-mbp.attlocal.net>
2020-07-15 13:35:02 -0500

    POST/PUT/DELETE method validation

--- RestAssured_maven/src/test/java/Test_Post.java
+++ RestAssured_maven/src/test/java/Test_Post.java
@@ -7,26 +7,55 @@ import static io.restassured.RestAssured.*;
 
 public class Test_Post {
 	
+//	@Test
+//	public void test_post() {
+//		JSONObject request = new JSONObject();
+//		
+//		request.put("name", "Ano");
+//		request.put("job", "learner");
+//		
+////		System.out.println(request);
+////		System.out.println(request.toJSONString());
+//		
+//		given().
+//			header("contentType", "application/JSON").
+//			contentType(ContentType.JSON).
+//			accept(ContentType.JSON).
+//			body(request.toJSONString()).
+//		when().
+//			post("https://reqres.in/api/users").
+//		then().
+//			statusCode(201);
+//		
+//	}
+	
+//	@Test
+//	public void test_put() {
+//		JSONObject request = new JSONObject();
+//		
+//		request.put("name","Mano");
+//		request.put("job", "student");
+//		
+//		given()
+//			.header("contentType", "application/JSON")
+//			.contentType(ContentType.JSON)
+//			.accept(ContentType.JSON)
+//			.body(request.toJSONString())
+//			.when()
+//				.put("https://reqres.in/api/users")
+//			.then()
+//				.statusCode(200);
+//	}
+	
 	@Test
-	public void test_post() {
-		JSONObject request = new JSONObject();
+	public void test_delete() {
 		
-		request.put("name", "Ano");
-		request.put("job", "learner");
-		
-//		System.out.println(request);
-//		System.out.println(request.toJSONString());
-		
-		given().
-			header("contentType", "application/JSON").
-			contentType(ContentType.JSON).
-			accept(ContentType.JSON).
-			body(request.toJSONString()).
-		when().
-			post("https://reqres.in/api/users").
-		then().
-			statusCode(201);
-		
+		when()
+			.delete("https://reqres.in/api/users")
+		.then()
+			.statusCode(204)
+			.log()
+			.all();
 	}
 
 }
